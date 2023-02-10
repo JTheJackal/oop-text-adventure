c90c5b740c83577b2a482b3feb77112d33ad7e75
Joshua Styles <styles@outlook.com>
2023-02-10 16:22:25 +0000

    Language singleton, House, Room and Player prepared.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OOPAdventure;
+
+Text.LoadLanguage(new English());
+
+Console.WriteLine(Text.Language.ChooseYourName);
+
+var name = Console.ReadLine();
+
+if(name == String.Empty)
+{
+    name = Text.Language.DefaultName;
+}
+
+var player = new Player(name);
+
+Console.WriteLine(Text.Language.Welcome, player.Name);
+
+var house = new House(player);
---
+++ Src/Text/English.cs
@@ -0,0 +1,15 @@
+﻿using OOPAdventure;
+
+public class English : Language
+{
+
+    public English()
+    {
+
+        ChooseYourName = "Hello, what is your name?";
+        DefaultName = "No Name";
+        Welcome = "Welcome {0} to your OOP Adventure";
+        DefaultRoomName = "Room {0} ({1},{2})";
+        DefaultRoomDescription = "You are in a room with doors to the {0}.";
+    }
+}
